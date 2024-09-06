fc35350a5ec931a0d137bd68d94d36e49ac06304
Matheus Augusto <129510488+maaat-aug@users.noreply.github.com>
2024-09-06 08:45:55 -0300

    Update Produto.cs

--- ProdutoCodigo/Models/Produto.cs
+++ ProdutoCodigo/Models/Produto.cs
@@ -1,10 +1,24 @@
-﻿namespace ProdutoCodigo.Models
+using System.ComponentModel.DataAnnotations;
+
+namespace ProdutoCodigo.Models
 {
     public class Produto
     {
+        [Required]
+        [MaxLength(100)]
+        [MinLength(3)]
         public string? descricao { get; set; }
+        [Required]
+        [MaxLength(10)]
         public double? preco { get; set; }
+        [Required]
+        [MaxLength(100)]
+        [MinLength(3)]
         public string? estoque { get; set; }
+
+        [Required]
+        [MaxLength(8)]
+        [MinLength(8)]
         public string codigoProduto { get; set; }
     }
 }
