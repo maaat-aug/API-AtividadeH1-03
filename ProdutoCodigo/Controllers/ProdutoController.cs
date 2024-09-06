c3038f9f3b48a2502ef15beeb1fd919fd7c84d44
matheus.teixeira <matheus.teixeira@ZE-TI16>
2024-09-06 08:35:37 -0300

    Adicionar arquivos de projeto.

---
+++ ProdutoCodigo/Controllers/ProdutoController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProdutoCodigo.Models;
+using System.Collections.Generic;
+
+namespace ProdutoCodigo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProdutoController : ControllerBase
+    {
+        private static List<Produto> codigoProdutos = new List<Produto>();
+
+        [HttpPost("RegistroCodigo")]
+        public IActionResult RegistroCodigo([FromBody] Produto produto)
+        {
+            const string numeros = "1234567890";
+
+            if (produto.codigoProduto.StartsWith("AAA-"))
+            {
+                for (int i = 4; i < produto.codigoProduto.Length; i++)
+                {
+                    if (numeros.IndexOf(produto.codigoProduto[i]) < 0)
+                    {
+                        return BadRequest("Codigo Produto Invalido");
+                    }
+                }
+
+                codigoProdutos.Add(produto);
+                return Ok("Produto registrado");
+            }
+
+            return BadRequest("Codigo Produto Invalido");
+        }
+    }
+}
---
+++ ProdutoCodigo/Models/Produto.cs
@@ -0,0 +1,10 @@
+﻿namespace ProdutoCodigo.Models
+{
+    public class Produto
+    {
+        public string? descricao { get; set; }
+        public double? preco { get; set; }
+        public string? estoque { get; set; }
+        public string codigoProduto { get; set; }
+    }
+}
